7154643b4b807b9cd491de206ac8c3cf4a9a05c2
arosecra <aaron.rosecrans@gmail.com>
2019-08-21 23:27:33 -0500

    gear update

--- addons/mbpetbar/mbpetbar.lua
+++ addons/mbpetbar/mbpetbar.lua
@@ -34,18 +34,14 @@ ashita.register_event('command', function(cmd, nType)
 end);
 
 ashita.register_event('render', function()
-    
-	imgui.SetNextWindowSize(200, 100, ImGuiSetCond_Always);
-    if (imgui.Begin('Pets') == false) then
-        imgui.End();
-        return;
-    end
-	
+
+	local petCount = 0;
 	
 	local party  = AshitaCore:GetDataManager():GetParty();
-    if (party == nil) then
-        return;
-    end
+	if (party == nil) then
+		return;
+	end
+	
 	
 	for i=1,5 do
 		local name = AshitaCore:GetDataManager():GetParty():GetMemberName(i)
@@ -57,24 +53,50 @@ ashita.register_event('render', function()
 				local petTargetIndex = entity.PetTargetIndex
 				if petTargetIndex ~= 0 then
 					local petEntity = GetEntity(entity.PetTargetIndex)
-					--print(petEntity.Name)
-					--print(petEntity.HealthPercent)
-					
-					imgui.Text(petEntity.Name .. "(" .. name .. ")");
-					imgui.Separator();
-					
-					-- Set the progressbar color for health..
-					imgui.PushStyleColor(ImGuiCol_PlotHistogram, 1.0, 0.61, 0.61, 0.6);
-					imgui.Text('HP:');
-					imgui.SameLine();
-					imgui.PushStyleColor(ImGuiCol_Text, 1.0, 1.0, 1.0, 1.0);
-					imgui.ProgressBar(tonumber(petEntity.HealthPercent) / 100, -1, 14);
-					imgui.PopStyleColor(2);
-										
-					imgui.End();
+					if petEntity ~= nil then
+						petCount = petCount + 1
+					end
 				end
 			end
 		end
-	end   
+	end 
+    
+	if petCount > 0 then
+		imgui.SetNextWindowSize(200, 100, ImGuiSetCond_Always);
+		if (imgui.Begin('Pets') == false) then
+			imgui.End();
+			return
+		end
+		
+		for i=1,5 do
+			local name = AshitaCore:GetDataManager():GetParty():GetMemberName(i)
+			local entityId = AshitaCore:GetDataManager():GetParty():GetMemberTargetIndex(i)
+			--print(entityId)
+			if entityId ~= 0 then
+				local entity = GetEntity(entityId)
+				if entity ~= nil then
+					local petTargetIndex = entity.PetTargetIndex
+					if petTargetIndex ~= 0 then
+						local petEntity = GetEntity(entity.PetTargetIndex)
+						--print(petEntity.Name)
+						--print(petEntity.HealthPercent)
+						
+						imgui.Text(petEntity.Name .. "(" .. name .. ")");
+						imgui.Separator();
+						
+						-- Set the progressbar color for health..
+						imgui.PushStyleColor(ImGuiCol_PlotHistogram, 1.0, 0.61, 0.61, 0.6);
+						imgui.Text('HP:');
+						imgui.SameLine();
+						imgui.PushStyleColor(ImGuiCol_Text, 1.0, 1.0, 1.0, 1.0);
+						imgui.ProgressBar(tonumber(petEntity.HealthPercent) / 100, -1, 14);
+						imgui.PopStyleColor(2);
+											
+						imgui.End();
+					end
+				end
+			end
+		end   
+	end
     
 end);
