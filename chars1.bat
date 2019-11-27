b7a25eba91d6d6e557a9b5fe21e8d5f279342084
arosecra <aaron.rosecrans@gmail.com>
2019-11-27 09:28:05 -0600

    added additional chars

---
+++ chars1.bat
@@ -0,0 +1,6 @@
+set PLAYONLINE_HOME=D:\Software\SteamLibrary\steamapps\common\FFXINA\SquareEnix\PlayOnlineViewer
+set ASHITA_HOME=D:\Software\Ashita
+copy "%PLAYONLINE_HOME%\usr\all\chars1.bin" "%PLAYONLINE_HOME%\usr\all\login_w.bin"
+del /Q %ASHITA_HOME%\config\boot\*
+copy "%ASHITA_HOME%\config\bootChars1\*" "%ASHITA_HOME%\config\boot"
+%ASHITA_HOME%\Ashita.exe
