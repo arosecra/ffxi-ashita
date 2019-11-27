4df23bcac2d18a75fb8abf60f22b230d0b061838
arosecra <aaron.rosecrans@gmail.com>
2019-11-27 10:49:58 -0600

    added additional chars

--- chars2.bat
+++ chars2.bat
@@ -2,5 +2,6 @@ set PLAYONLINE_HOME=D:\Software\SteamLibrary\steamapps\common\FFXINA\SquareEnix\
 set ASHITA_HOME=D:\Software\Ashita
 copy "%PLAYONLINE_HOME%\usr\all\chars2.bin" "%PLAYONLINE_HOME%\usr\all\login_w.bin"
 del /Q %ASHITA_HOME%\config\boot\*
+mkdir %ASHITA_HOME%\config\boot\
 copy "%ASHITA_HOME%\config\bootChars2\*" "%ASHITA_HOME%\config\boot"
 %ASHITA_HOME%\Ashita.exe
